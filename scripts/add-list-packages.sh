2668dc6cf4485e037dedeab41b3ebb8d0725cd2a
Estation User <esuser@h05-dev-vm22.ies.jrc.it>
2014-01-13 18:12:01 +0100

    Add the option of having .deb from PPA

--- scripts/add-list-packages.sh
+++ scripts/add-list-packages.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # this script should be run to add a complete list of packages to a repository
 # The name of the list is passed as argument
+# Script can also add a specific PPA, in which case in the list the syntax below should be used:
 
-# > add-list-packages.sh  list_filename.txt
+# package_std1
+# package_std2
+# package_ppa1<<<ppa_definition
+
+# Usage: add-list-packages.sh  list_filename.txt
 
 # the packages and all his dependency will be added to the
 # NOTE that: 1. a subdirectory is created/used under PACKAGE_REPO
@@ -60,7 +65,19 @@ LIST_PCKGS=($(cat $LIST_FILE))
 
 for package in ${LIST_PCKGS[@]}
 do
-	./add-package.sh ${package}
+	# Check if is commented
+	if [[ ! ${package} =~ '#' ]];
+	then 
+  	  # Check if a PPA is defined
+	  if [[ ${package} =~ '<<<' ]]; 
+	  then 
+		name=${package%%<<<*}
+		ppa=${package##*<<<}
+		./add-package-ppa.sh ${name} ${ppa}
+	  else
+		./add-package.sh ${package}
+	  fi
+	fi
 done
 
 
