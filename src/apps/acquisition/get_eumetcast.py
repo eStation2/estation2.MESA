c1cf88d5af226e3445ee385926be645b7042abba
Estation User <esuser@h05-dev-vm19.ies.jrc.it>
2014-05-15 17:00:32 +0200

    For Acquisition Testing

--- src/apps/acquisition/get_eumetcast.py
+++ src/apps/acquisition/get_eumetcast.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 #
 #	purpose: Define the get_eumetcast service
 #	author:  M.Clerici
@@ -5,12 +7,135 @@
 #   descr:	 Reads the definition from eStation DB and execute the copy to local disk
 #	history: 1.0
 
+#import os, sys, re, signal, commands, time, datetime
+#from time import sleep
+#from lxml import etree
+#import pickle
+
 
 from config.es2 import *
 
 logger = log.my_logger(__name__)
 
+# Defined in lib.python.es_constants.py
 #eumetcast_files_dir = '/home/esuser/my_eumetcast_dir/'
-#internet_files_dir = '/home/esuser/my_data_ingest_dir/'
-data_dir_in = test_data_dir_in
-data_dir_out = test_data_dir_out
+#ingest_server_in_dir = '/home/esuser/my_data_ingest_dir/'
+input_dir = eumetcast_files_dir
+output_dir = ingest_server_in_dir
+sleep = poll_frequency
+echo_query = False
+
+
+def find_files(directory, pattern):
+    lst = []
+    for root, dirs, files in os.walk(directory):
+        for basename in files:
+            if re.search(pattern, basename):
+                fn = os.path.join(root, basename)
+                lst.append(fn)
+    return lst
+
+
+def match_curlst(lst, pattern):
+    curentlst = []
+    for entry in lst:
+        if re.search(pattern, os.path.basename(entry)):
+            curentlst.append(entry)
+    return curentlst
+
+
+processed_list = []
+processlist = get_eumetcast_processed_list
+
+
+def signal_handler(signal, frame):
+    process_files = open(processlist, 'wb')
+    pickle.dump(processed_list, process_files)
+    process_file.close()
+    print 'Exit ' + sys.argv[0]
+    logger.info("Stopping the service.")
+    sys.exit(0)
+
+
+signal.signal(signal.SIGTERM, signal_handler)
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGILL, signal_handler)
+
+logger.info("Starting retrieving EUMETCast data.")
+
+
+logger.debug("Check if the EUMETCast input directory : %s exists.", input_dir)
+if not os.path.exists(input_dir):
+    logger.error("The EUMETCast input directory : %s is not yet mounted.", input_dir)
+
+logger.debug("Check if the Ingest Server output directory : %s exists.", output_dir)
+if not os.path.exists(output_dir):
+    logger.fatal("The Ingest Server output directory : %s doesn't exists.", output_dir)
+    # TODO Jurvtk: Create the Ingest Server output directory if it doesn't exist!
+    exit(1)
+
+if not os.path.exists(processed_list_dir):
+    os.mkdir(processed_list_dir)
+
+logger.debug("Loading the processed file list.")
+if os.path.exists(processlist):
+    try:
+        process_file = open(processlist, 'r')
+        processed_list = pickle.load(process_file)
+        logger.debug("Processed file list loaded.")
+    except:
+        logger.warning("Processed file can't be loaded, the file will be removed.")
+        os.remove(processlist)
+else:
+    # Create an empty file in the tmp dir
+    open(processlist, 'a').close()
+
+    
+while 1:
+    try:
+        time_sleep = sleep
+        logger.debug("Sleep time set to : %s.", time_sleep)
+    except:
+        logger.warning("Sleep time not defined. Setting to default=1min. Continue.")
+        time_sleep = 60
+
+    try:
+        logger.debug("Reading active EUMETCAST data sources from database")
+        eumetcast_sources_list = querydb.get_eumetcast_sources(echo=echo_query)
+        for eumetcast_source in eumetcast_sources_list:
+            logger.debug("Create current list of file to process for trigger %s.", eumetcast_source.eumetcast_id)
+            current_list = find_files(input_dir, eumetcast_source.filter_expression_jrc)
+            myprocesslst = match_curlst(processed_list, eumetcast_source.filter_expression_jrc)
+            listtoprocess = []
+            listtoprocess = set(current_list).symmetric_difference(set(myprocesslst))
+            if listtoprocess != set([]):
+                logger.debug("Loop on the found files.")
+                for filename in list(listtoprocess):
+                    if os.path.isfile(os.path.join(input_dir, filename)):
+                        if os.stat(os.path.join(input_dir, filename)).st_mtime < int(time.time()):
+                            logger.debug("Processing file: "+os.path.basename(filename))
+                            if commands.getstatusoutput("cp " + filename + " " + output_dir + '/' + os.path.basename(filename))[0] == 0:
+                                logger.info("File %s copied.", filename)
+                                processed_list.append(filename)
+                            else:
+                                logger.warning("Problem while copying file: %s.", filename)
+                    else:
+                        logger.error("File %s removed by the system before being processed.", filename)
+            else:
+                logger.debug("Nothing to process - go to next trigger.")
+                pass
+
+        for infile in processed_list:
+            if not os.path.exists(infile):
+                processed_list.remove(infile)
+
+        process_file = open(processlist, 'wb')
+        pickle.dump(processed_list, process_file)
+        process_file.close()
+        sleep(float(time_sleep))
+    except Exception, e:
+        logger.fatal(str(e))
+        exit(1)
+exit(0)
+
+
--- src/lib/python/.test/testquerydb.py
+++ src/lib/python/.test/testquerydb.py
@@ -5,6 +5,25 @@ import lib.python.querydb as querydb
 db = querydb.connect_db()
 
 product = 'vgt_ndvi'
+version = 'undefined'
+args = {"productcode": product, "version": version}
+result = querydb.get_ingestion_product(echo=True, **args)
+print "all records:"
+result2 = querydb.get_ingestion_product(allrecs=True, echo=True)
+
+
+
+from sqlalchemy import func
+session = db.session
+where = db.ingestion.activated
+ingestion_product = session.query(db.ingestion.productcode,
+                                  db.ingestion.version,
+                                  func.count(db.ingestion.subproductcode), ). \
+    group_by(db.ingestion.productcode, db.ingestion.version).filter(where)
+print ingestion_product.all()
+
+
+product = 'vgt_ndvi'
 subproduct = 'ndv'
 pkey = {"productcode": product, "subproductcode": subproduct}
 product_info = querydb.get_product_out_info(echo=True, **pkey)
@@ -30,72 +49,72 @@ product_in_params = {"in_data_type": product_in_info.data_type_id,
 print product_in_params
 
 
-#pkey = {"productcode": "vgt_ndvi", "subproductcode": "ndv"}
-#products = querydb.get_product_info(allrecs=True, echo=True)
-#product = querydb.get_product_info(echo=True, **pkey)
+pkey = {"productcode": "vgt_ndvi", "subproductcode": "ndv"}
+products = querydb.get_product_info(allrecs=True, echo=True)
+product = querydb.get_product_info(echo=True, **pkey)
 
-#products = querydb.get_product_native(allrecs=True, echo=True)
-#product = querydb.get_product_native('ndvi', echo=True)
-#
-#eumetcasts = querydb.get_eumetcast(allrecs=True, echo=True)
-#eumetcast = querydb.get_eumetcast('EO:EUM:DAT:SPOT:S10NDVI', echo=True)
-#
-#internet_ds = querydb.get_internet(allrecs=True, echo=True)
-#internetds = querydb.get_internet('MOD09GA_Africa', echo=True)
+products = querydb.get_product_native(allrecs=True, echo=True)
+product = querydb.get_product_native('ndvi', echo=True)
 
-#mapsets = querydb.get_mapset(allrecs=True, echo=True)
-#mapset = querydb.get_mapset('WGS84_Africa_km', echo=True)
+eumetcasts = querydb.get_eumetcast(allrecs=True, echo=True)
+eumetcast = querydb.get_eumetcast('EO:EUM:DAT:SPOT:S10NDVI', echo=True)
 
-#pkey = {"productcode": "ndvi", "subproductcode": "ndv"}
-#ingestions = querydb.get_ingestion(allrecs=True, echo=True)
-#ingestion = querydb.get_ingestion(echo=True, **pkey)
+internet_ds = querydb.get_internet(allrecs=True, echo=True)
+internetds = querydb.get_internet('MOD09GA_Africa', echo=True)
 
-#pkey = {"productcode": "ndvi", "subproductcode": "ndv"}
-#datasource_descr = querydb.get_datasource_descr(echo=True, source_type='EUMETCAST', source_id='EO:EUM:DAT:SPOTS10NDVI')
+mapsets = querydb.get_mapset(allrecs=True, echo=True)
+mapset = querydb.get_mapset('WGS84_Africa_km', echo=True)
 
+pkey = {"productcode": "ndvi", "subproductcode": "ndv"}
+ingestions = querydb.get_ingestion(allrecs=True, echo=True)
+ingestion = querydb.get_ingestion(echo=True, **pkey)
 
-#row = db.datetype.filter_by(date_type='HHMM').one()
-#print row
-#
-
-#products = db.product.first()
-#products = db.entity("product").first()
-#print products
-#
-#products = db.product.order_by(db.product.productcode).all()
-#for row in products:
-#    print row
-#
-#
-#from sqlalchemy import or_, and_, desc
-#where = and_(db.product.productcode=='ndvi', db.product.subproductcode=='ndv')
-#products = db.product.filter(where).order_by(desc(db.product.productcode)).all()
-#for row in products:
-#    print row
-
-#product = db.product.get('ndvi') # must be single primary key field
-#print product
-#product = db.product.filter_by(productcode='ndvi').one()
-#print product
+pkey = {"productcode": "ndvi", "subproductcode": "ndv"}
+datasource_descr = querydb.get_datasource_descr(echo=True, source_type='EUMETCAST', source_id='EO:EUM:DAT:SPOTS10NDVI')
 
 
-#datetypes = querydb.read_datetypes()
-#for row in datetypes:
-#    print row
-#
-#querydb.create_datetype('JURRIAAN','Dit is een test 123')
-#datetypes = querydb.read_datetypes()
-#for row in datetypes:
-#    print row
-#
-#querydb.update_datetype('TESTJUR','You can similarly update multiple rows at once.')
-#datetypes = querydb.read_datetypes()
-#for row in datetypes:
-#    print row
-#
-#querydb.delete_datetype('JURRIAAN')
-#datetypes = querydb.read_datetypes()
-#for row in datetypes:
-#    print row
+row = db.datetype.filter_by(date_type='HHMM').one()
+print row
+
+
+products = db.product.first()
+products = db.entity("product").first()
+print products
+
+products = db.product.order_by(db.product.productcode).all()
+for row in products:
+    print row
+
+
+from sqlalchemy import or_, and_, desc
+where = and_(db.product.productcode=='ndvi', db.product.subproductcode=='ndv')
+products = db.product.filter(where).order_by(desc(db.product.productcode)).all()
+for row in products:
+    print row
+
+product = db.product.get('ndvi') # must be single primary key field
+print product
+product = db.product.filter_by(productcode='ndvi').one()
+print product
+
+
+datetypes = querydb.read_datetypes()
+for row in datetypes:
+    print row
+
+querydb.create_datetype('JURRIAAN','Dit is een test 123')
+datetypes = querydb.read_datetypes()
+for row in datetypes:
+    print row
+
+querydb.update_datetype('TESTJUR','You can similarly update multiple rows at once.')
+datetypes = querydb.read_datetypes()
+for row in datetypes:
+    print row
+
+querydb.delete_datetype('JURRIAAN')
+datetypes = querydb.read_datetypes()
+for row in datetypes:
+    print row
 
 
