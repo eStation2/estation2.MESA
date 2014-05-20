ace59baabbf81433ec37052c42682122f5d1a850
Estation User <esuser@h05-dev-vm26.jrc.it>
2014-05-20 15:30:18 +0200

    Fix

--- build/scripts/download-list-python.sh
+++ build/scripts/download-list-python.sh
@@ -39,7 +39,8 @@ echo "Package: " ${package}
         # Create symbolic link version independent
         pack_fullname=$(ls $PYTHON_PACKAGE_REPO/${package}*)
         pack_no_release=$(echo ${package_fullname} | sed 's/[-\.][0-9]//g')
-		ln -fs "${package_fullname}" "${pack_no_release}"
+
+		echo "ln -fs "${package_fullname}" "${pack_no_release}""
 	fi
 done
 
