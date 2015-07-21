484e3f2734b348c20db38547ef15d1f0333c27dd
Estation User <esuser@h05-dev-vm26.jrc.it>
2015-07-21 16:13:55 +0200

    Replace definitions at package generation (see #42

--- build/packages/eStation-Apps/build/build-deb.sh
+++ build/packages/eStation-Apps/build/build-deb.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# Package description
+#
+#   Script to create the eStation2-apps debian package
+#   It also replaces placeholders in pre/postinst files
+
+# Source the eStation2 specific definitions
+
+. ./eStation2_config
+
 Project=eStation-
 Name=eStation-Apps
-Version=2.0.1
-Release=1
-
-# this is the destination folder for app sorces, this is specific for this package
-APP_SOURCE_DESTINATION=/srv/www/eStation2
+Version=2.0.4
+Release=20
 
 #this is the deb package name
 PACKAGE_DEB_NAME=${Name}-${Version}-${Release}.deb
@@ -17,44 +21,67 @@ PACKAGE_DEB_NAME=${Name}-${Version}-${Release}.deb
 . ../../../scripts/config
 . ../../debianbuild
 
-
-## Tests before 'local-build'
-#status=($(git status | grep modified))
-#if [ ! -z ${status[0]} ]; then
-#    echo "ERROR - Local modifications exist: commit/pull/push first"
-#    git status
-#    exit
-#fi
-
-
 #package destination dir usually ${PACKAGE_BUILD_DIR_DEB} but it can be a subdirectory with timestamp
 PACKAGE_DESTINATION_DIR=${PACKAGE_BUILD_DIR_DEB}
 
 set -e
 
 localdir=$(pwd)
+
 # where the package is assembled
-BUILDdir=${PACKAGE_BUILD_DIR_TMP}/${Name}-${Version}
+BUILDdir=${PACKAGE_BUILD_DIR_TMP}/${Name}-${ESTATION2_VERSION}
 
 # Cleaning
 deb_clean ${BUILDdir}
 
-mkdir -p ${BUILDdir}/${APP_SOURCE_DESTINATION}
+mkdir -p ${BUILDdir}/${ESTATION2_BASE_DIR_SYSTEM}
 
 cp -r ../src/* ${BUILDdir}/
-cp -r ${ESTATION_SRC_DIR}/* ${BUILDdir}/${APP_SOURCE_DESTINATION}
+cp -r ${ESTATION_SRC_DIR}/* ${BUILDdir}/${ESTATION2_BASE_DIR_SYSTEM}
 cp -r ./DEBIAN ${BUILDdir}
 
+############################
+# Parsing preinst/postinst
+############################
+# Replace variables in preinst and postinst
+for file in ${BUILDdir}/DEBIAN/preinst ${BUILDdir}/DEBIAN/postinst ${BUILDdir}/DEBIAN/control
+do
+for var in 'ESTATION2_VERSION' 'ESTATION2_RELEASE' 'ESTATION2_UBUNTU' 'ESTATION2_MAIN_USER' 'ESTATION2_THEM_USER' 'ESTATION2_MAIN_GROUP' 'ESTATION2_BASE_DIR_SYSTEM' 'ESTATION2_LOCAL_DIR' 'ESTATION2_DIR_LOG' 
+do
+    placeholder="<${var}>"
+    value=$(eval "echo \$${var}")
+    perl -p -i -e "s#${placeholder}#${value}#g" $file
+done
+done
+for file in ${BUILDdir}/DEBIAN/preinst ${BUILDdir}/DEBIAN/postinst ${BUILDdir}/DEBIAN/control
+do
+for var in 'ESTATION2_DIR_LISTS' 'ESTATION2_DIR_SETTINGS' 'ESTATION2_DIR_LIST_EUM' 'ESTATION2_DIR_LIST_INT' 'ESTATION2_DIR_DATABASE' 
+do
+    placeholder="<${var}>"
+    value=$(eval "echo \$${var}")
+    perl -p -i -e "s#${placeholder}#${value}#g" $file
+done
+done
+
+for file in ${BUILDdir}/DEBIAN/preinst ${BUILDdir}/DEBIAN/postinst ${BUILDdir}/DEBIAN/control
+do
+for var in 'ESTATION2_DIR_DATABASE' 'ESTATION2_TEMP_DIR' 'ESTATION2_DIR_SERVICES' 'ESTATION2_DIR_PROCESS' 'ESTATION2_BASE_DIR_DATA' 'ESTATION2_INST_LOG_DIR' 'ESTATION2_PYTHON_DIST_DIR'
+do
+    placeholder="<${var}>"
+    value=$(eval "echo \$${var}")
+    perl -p -i -e "s#${placeholder}#${value}#g" $file
+done
+done
 
 ############################
 # Parsing for build
 ############################
 ver=$(echo ${Version}|sed 's/\./_/g')
-perl -p -i -e "s#<INSTALLDIR>#${installdir}#g" ${BUILDdir}/DEBIAN/*
-perl -p -i -e "s#<VINSTALLDIR>#${vinstalldir}#g" ${BUILDdir}/DEBIAN/*
-perl -p -i -e "s/<VERSION>/${Version}/g" ${BUILDdir}/DEBIAN/*
-perl -p -i -e "s/<RELEASE>/${Release}/g" ${BUILDdir}/DEBIAN/*
-perl -p -i -e "s/<NAME>/${Name}/g" ${BUILDdir}/DEBIAN/*
+#perl -p -i -e "s#<INSTALLDIR>#${installdir}#g" ${BUILDdir}/DEBIAN/*
+#perl -p -i -e "s#<VINSTALLDIR>#${vinstalldir}#g" ${BUILDdir}/DEBIAN/*
+#perl -p -i -e "s/<VERSION>/${Version}/g" ${BUILDdir}/DEBIAN/*
+#perl -p -i -e "s/<RELEASE>/${Release}/g" ${BUILDdir}/DEBIAN/*
+#perl -p -i -e "s/<NAME>/${Name}/g" ${BUILDdir}/DEBIAN/*
 cd ${BUILDdir}
 find . -type f|grep -v svn |grep -v DEBIAN > ${BUILDdir}/DEBIAN/conffiles
 cd -
