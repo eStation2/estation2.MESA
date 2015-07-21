5c9de107fedb18ccc7b857e82c03969af02fd8a3
Estation User <esuser@h05-dev-vm26.jrc.it>
2015-07-21 11:46:27 +0200

    Restore build machine

--- build/CD_IMAGE/eStation/setup/late-command.sh
+++ build/CD_IMAGE/eStation/setup/late-command.sh
@@ -2,6 +2,22 @@
 # this is the late command script to be run by install preseed
 
 # install pygrib python module 
+pip install /media/cdrom/eStation/Python-Modules/ruffus.tar.gz
 pip install /media/cdrom/eStation/Python-Modules/pygrib.tar.gz
+pip install /media/cdrom/eStation/Python-Modules/SQLAlchemy.tar.gz
 pip install /media/cdrom/eStation/Python-Modules/sqlsoup.tar.gz
+pip install /media/cdrom/eStation/Python-Modules/web.py.tar.gz
+pip install /media/cdrom/eStation/Python-Modules/psutil.tar.gz
+pip install /media/cdrom/eStation/Python-Modules/reloader.tar.gz
+pip install /media/cdrom/eStation/Python-Modules/greenwich.tar.gz
+
+# create Thematic User
+
+username=analyst
+password=mesa2015
+fullname='eStation Thematic User'
+pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+useradd -c "$fullname" -s /bin/bash -m -p $pass $username
+
+
 
