5af2684d419f8fd338760a716b3e848e63b1333f
clerima <marco.clerici@jrc.ec.europa.eu>
2015-09-15 18:38:33 +0200

    Minor

--- src/apps/processing/test_processing.py
+++ src/apps/processing/test_processing.py
@@ -1,10 +1,15 @@
 __author__ = "Marco Clerici"
 
 # General definitions/switches
-args = {'pipeline_run_level':0, \
+args = {'pipeline_run_level':4, \
         'pipeline_run_touch_only':0, \
-        'pipeline_printout_level':6, \
-        'pipeline_printout_graph_level': 0}
+        'pipeline_printout_level':0, \
+        'pipeline_printout_graph_level': 0, \
+        'prod': 'vgt-ndvi',\
+        'starting_sprod':'ndv',\
+        'mapset': 'SPOTV-Africa-1km',\
+        'version':'sv2-pv2.1'
+        }
 
 #from apps.processing.processing_modis_sst import *
 #from apps.processing.processing_std_precip import *
@@ -12,4 +17,7 @@ from apps.processing.processing_std_ndvi import *
 #   ---------------------------------------------------------------------
 #   Run the pipeline
 
-processing_std_ndvi(**args)
+#processing_std_ndvi(**args)
+#processing_std_ndvi_stats_only(**args)
+processing_std_ndvi_prods_only(**args)
+#processing_std_ndvi_prods_only(**args)
