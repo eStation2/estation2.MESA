f62acfc2d0a1651ce12018f89a7db0ea458322e4
root <root@MESA-PC2.(none)>
2015-10-20 01:24:04 +0200

    Conversion to Spirits Format

--- src/apps/es2system/convert_2_spirits.py
+++ src/apps/es2system/convert_2_spirits.py
@@ -10,6 +10,7 @@ import os
 import csv
 import operator
 from config import es_constants
+import fnmatch
 
 # Import eStation2 modules
 from lib.python import es_logging as log
@@ -17,15 +18,17 @@ from lib.python import functions
 from database import querydb
 logger = log.my_logger(__name__)
 
-metadata_spirits= {'prod_values': '{NDVI-toc, -, 0, 250, 0, 250, -0.08, 0.004}',
-                   'flags': '{251=missing, 252=cloud, 253=snow, 254=sea, 255=back, 254=back}', \
-                   'data_ignore_value':'255', \
-                   'days': 10, \
-                   'sensor_type':'VEGETATION', \
-                   'comment':'My comment', \
-                   'sensor_filename_prefix':'', \
+naming_spirits = { 'sensor_filename_prefix':'', \
                    'frequency_filename_prefix':'', \
                    'pa_filename_prefix':''}
+
+metadata_spirits= {'prod_values': '',
+                   'flags': '', \
+                   'data_ignore_value':'', \
+                   'days': 0, \
+                   'sensor_type':'', \
+                   'comment':''}
+
 
 def write_properties(filename,dictionary):
     """ Writes the provided dictionary in key-sorted order to a properties file with each line of the format key=value
@@ -67,16 +70,16 @@ def append_to_header_file(header_file, metadata_spirit):
          logger.error('The header file does not exist : : %s' % header_file)
 
 # Convert a single file
-def convert_geotiff_file(input_file, output_dir, str_date, md_spirit):
+def convert_geotiff_file(input_file, output_dir, str_date, naming_spirits, metadata_spirits):
 
     extension_bin = '.bin'
     extension_hdr = '.hdr'
 
     # Define output filename
-    output_base_name = md_spirit['sensor_filename_prefix']+'_'+\
-                  md_spirit['frequency_filename_prefix']+'_'+\
-                  str_date+'_'+ \
-                  md_spirit['pa_filename_prefix']
+    output_base_name = naming_spirits['sensor_filename_prefix']+\
+                  naming_spirits['frequency_filename_prefix']+\
+                  str_date+ \
+                  naming_spirits['pa_filename_prefix']
 
     output_path = output_dir+os.path.sep+output_base_name+extension_bin
 
@@ -99,6 +102,51 @@ def convert_geotiff_file(input_file, output_dir, str_date, md_spirit):
     if status:
          logger.error('Error in modifying SPIRITS header: %s' % header_file_name)
 
-def convert_driver():
+def convert_driver(output_dir=None):
+
+    # Definitions
+    input_dir = es_constants.es2globals['processing_dir']
+
     # Read the spirits table and convert all existing files
-    pass
+    spirits_list = querydb.get_spirits()
+    for entry in spirits_list:
+
+        product_code = entry['productcode']
+        sub_product_code = entry['subproductcode']
+        version = entry['version']
+        mapset = entry['mapsetcode']
+
+        # Prepare the naming dict
+        naming_spirits = { 'sensor_filename_prefix':entry['sensor_filename_prefix'], \
+                           'frequency_filename_prefix':entry['frequency_filename_prefix'], \
+                           'pa_filename_prefix':entry['product_anomaly_filename_prefix']}
+
+        metadata_spirits= {'prod_values': entry['prod_values'],
+                           'flags': entry['flags'], \
+                           'data_ignore_value':entry['data_ignore_value'], \
+                           'days': entry['days'], \
+                           'sensor_type':entry['sensor_type'], \
+                           'comment':entry['comment']}
+
+        # Input subdir
+        subdir = product_code + os.path.sep + version + os.path.sep
+        if mapset is not None:
+            mapset+=mapset+ os.path.sep
+
+        # Get the list of input products
+        match_regex='*' + product_code + '_' + \
+                    sub_product_code + '_*_' +\
+                    version +'*'
+
+        logger.debug("Looking in directory: %s" % input_dir)
+
+        # Get the list of matching files in /data/ingest
+        available_files=[]
+        for root, dirnames, filenames in os.walk(input_dir+subdir):
+            for filename in fnmatch.filter(filenames, match_regex):
+                available_files.append(os.path.join(root, filename))
+
+        if len(available_files) > 0:
+            for input_file in available_files:
+                str_date = functions.get_date_from_path_filename(os.path.basename(input_file))
+                convert_geotiff_file(input_file, output_dir, str_date, naming_spirits, metadata_spirits)
