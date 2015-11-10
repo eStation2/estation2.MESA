4e7c2f956ed0112eee35c833c472847abe6ccd4f
clerima <marco.clerici@jrc.ec.europa.eu>
2015-11-10 08:57:08 +0100

    Convert new products

--- src/apps/tools/convert_from_1.0.py
+++ src/apps/tools/convert_from_1.0.py
@@ -8,12 +8,33 @@ __author__ = 'analyst'
 #	history: 1.0
 #
 
-from apps.acquisition.ingestion import *
+from apps.acquisition import ingestion
 from glob import *
+from lib.python import es_logging as log
+import os
 logger = log.my_logger(__name__)
 
+def ingest_from_vers_1_vgt_dmp(input_file,dry_run=False):
 
-def ingest_from_vers_1(input_file,dry_run=False):
+    in_date = os.path.basename(input_file).split('_')[0]
+
+    product_def = {'productcode': 'vgt-dmp',
+                   'subproductcode': 'dmp',
+                   'version': 'sv-v1',
+                   'type': 'Ingest'}
+
+    product_in_info= {'scale_factor': 0.01,
+                      'scale_offset': 0.0,
+                      'no_data': -32768,
+                      'mask_min': None,
+                      'mask_max': None,
+                      'data_type_id': 'Ingest'}
+
+    target_mapset= 'SPOTV-Africa-1km'
+    ingestion.ingest_file_vers_1_0(input_file, in_date, product_def, target_mapset, logger, product_in_info, echo_query=False)
+    return 0
+
+def ingest_from_vers_1_lsasaf_et(input_file,dry_run=False):
 
 
     in_date = os.path.basename(input_file).split('_')[0]
@@ -36,7 +57,7 @@ def ingest_from_vers_1(input_file,dry_run=False):
 
 if __name__=='__main__':
 
-    input_dir = '/data/from_1_0/'
+    input_dir = '/data/from_1_0/vgt-dmp/'
     files = glob(input_dir+'*')
     for infile in files:
-        result = ingest_from_vers_1(infile,dry_run=False)
+        result = ingest_from_vers_1_vgt_dmp(infile,dry_run=False)
