b176eec322ffa499fe8d60020b1d8ecb95b64d78
clerima <marco.clerici@jrc.ec.europa.eu>
2015-11-10 08:57:38 +0100

    Convert new products

--- src/apps/es2system/Test/test_spirits.py
+++ src/apps/es2system/Test/test_spirits.py
@@ -21,18 +21,20 @@ class TestSpirits(TestCase):
                            'frequency_filename_prefix':'t', \
                            'pa_filename_prefix':'k'}
 
-        metadata_spirits= {'prod_values': '{NDVI, -, 0, 1000, 0, 1000, 0, 0.001}',
+        metadata_spirits= {'values': '{NDVI, -, 0, 1000, 0, 1000, 0, 0.001}',
                            'flags': ' {-32768=missing}', \
                            'data_ignore_value':'', \
                            'days': 10, \
                            'sensor_type':'SPOT VGT-PROBA V', \
-                           'comment':'Smoothed NDVI v2.1, JRC eStation2.0'}
+                           'comment':'Smoothed NDVI v2.1, JRC eStation2.0',
+                           'date':''}
 
         input_file='/data/processing/vgt-ndvi/sv2-pv2.1/SPOTV-Africa-1km/tif/ndv/20110101_vgt-ndvi_ndv_SPOTV-Africa-1km_sv2-pv2.1.tif'
         output_dir = '/data/temp/spirits'
         str_date = '20110101'
+        metadata_spirits['date']=str_date
         conv.convert_geotiff_file(input_file, output_dir, str_date, naming_spirits, metadata_spirits)
 
     def test_driver(self):
-        output_dir = '/data/temp/spirits'
+        output_dir = '/data/spirits/'
         conv.convert_driver(output_dir)
