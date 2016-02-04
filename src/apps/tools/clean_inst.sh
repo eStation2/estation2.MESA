0d5b1041ac75d823cf45e68cfbaf2e13ae47bf81
clerima <marco.clerici@jrc.ec.europa.eu>
2016-02-04 09:20:34 +0100

    Small script to clean .rpm installation before re-installing

---
+++ src/apps/tools/clean_inst.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+yum remove eStation2-Docs.x86_64
+yum remove eStation2-Layers.x86_64
+yum remove eStation2-Apps.x86_64
+
+rm -fr /var/www/eStation2*
+rm -fr /tmp/eStation2/*
+rm -fr /eStation2/*
+
+/etc/init.d/postgresql-9.3 restart
+sleep 1
+/etc/init.d/postgresql-9.3 restart
+sleep 1
+/etc/init.d/postgresql-9.3 restart
+dropdb -U estation estationdb
+
+dropdb -U bucardo bucardo
+
