483bd0722799dd202b3f56f72c4ba367e951e2f7
clerima <marco.clerici@jrc.ec.europa.eu>
2016-04-15 17:33:26 +0200

    Apply to modis-firms as well

--- src/apps/tools/convert_from_1.0.py
+++ src/apps/tools/convert_from_1.0.py
@@ -52,12 +52,33 @@ def ingest_from_vers_1_lsasaf_et(input_file,dry_run=False):
                       'data_type_id': 'Ingest'}
 
     target_mapset= 'MSG-satellite-3km'
-    ingest_file_vers_1_0(input_file, in_date, product_def, target_mapset, logger, product_in_info, echo_query=False)
+    ingestion.ingest_file_vers_1_0(input_file, in_date, product_def, target_mapset, logger, product_in_info, echo_query=False)
+    return 0
+
+def ingest_from_vers_1_modis_firms(input_file,dry_run=False):
+
+
+    in_date = os.path.basename(input_file).split('_')[0]
+
+    product_def = {'productcode': 'modis-firms',
+                   'subproductcode': '10dcount',
+                   'version': 'v5.0',
+                   'type': 'Derived'}
+
+    product_in_info= {'scale_factor': 1.0,
+                      'scale_offset': 0.0,
+                      'no_data': 0,
+                      'mask_min': None,
+                      'mask_max': None,
+                      'data_type_id': 'Derived'}
+
+    target_mapset= 'SPOTV-Africa-1km'
+    ingestion.ingest_file_vers_1_0(input_file, in_date, product_def, target_mapset, logger, product_in_info, echo_query=False)
     return 0
 
 if __name__=='__main__':
 
-    input_dir = '/data/from_1_0/vgt-dmp/'
+    input_dir = '/data/from_1_0/modis-firms/10dcount/2014*'
     files = glob(input_dir+'*')
     for infile in files:
-        result = ingest_from_vers_1_vgt_dmp(infile,dry_run=False)
+        result = ingest_from_vers_1_modis_firms(infile,dry_run=False)
