f55d9d9f66f6c213d21900bf7c51b4dee73aebc4
clerima <marco.clerici@jrc.ec.europa.eu>
2016-07-08 16:59:50 +0200

    Comment the file/directory option, to be restored when the GUI functionality for ingesting archives is implemented

--- src/apps/tools/create_archive_from_request.py
+++ src/apps/tools/create_archive_from_request.py
@@ -20,37 +20,42 @@ logger = log.my_logger(__name__)
 if __name__=='__main__':
 
     debug = 0
+    debug_req_file = '/home/adminuser/fewsnet-rfe_2.0_all_enabled_mapsets_2016-05-02_1156.req'
 
     if not debug:
     # Parse the input
         parser = argparse.ArgumentParser(description='Create bsx archive from request')
         parser.add_argument('req_file', type=str, help='Name of the request file/dir to process')
-        parser.add_argument('--file', type=str, help='The passed arg is full filename')
-        parser.add_argument('--directory', type=str, help='The passed arg is directory name')
+
+        # Commented FTTB: to be implemented through a GUI
+        # parser.add_argument('--file', type=str, help='The passed arg is full filename')
+        # parser.add_argument('--directory', type=str, help='The passed arg is directory name')
 
         args = parser.parse_args()
         req_file= args.req_file
 
-        if args.file:
-            logger.info('Working on a .req file: %s' % req_file)
-            if os.path.isfile(req_file):
-                requests.create_archive_from_request(req_file)
-            else:
-                logger.error('Req. file does not exist: %s' % req_file)
+        # Commented FTTB: to be implemented through a GUI
+        # if args.file:
+        logger.info('Working on a .req file: %s' % req_file)
+        if os.path.isfile(req_file):
+            requests.create_archive_from_request(req_file)
+        else:
+            logger.error('Req. file does not exist: %s' % req_file)
 
-        if args.directory:
-            logger.info('Working on a directory: %s' % req_file)
-            if os.path.isdir(req_file):
-                req_files = glob.glob(req_file+'*.req')
-                for my_req in req_files:
-                    requests.create_archive_from_request(my_req)
-            else:
-                logger.error('Req. file does not exist: %s' % req_file)
+        # Commented FTTB: to be implemented through a GUI
+        # if args.directory:
+        #     logger.info('Working on a directory: %s' % req_file)
+        #     if os.path.isdir(req_file):
+        #         req_files = glob.glob(req_file+'*.req')
+        #         for my_req in req_files:
+        #             requests.create_archive_from_request(my_req)
+        #     else:
+        #         logger.error('Req. file does not exist: %s' % req_file)
             # Get list of all req. in the dir
-    # else:
-    # # # For debugging
-    #     req_file= '/data/User_Requests/Yoseph-ICPAC/missing_data/vgt-ndvi_sv2-pv2.1_all_enabled_mapsets_2016-05-26_1334.req'
-    #     if os.path.isfile(req_file):
-    #         requests.create_archive_from_request(req_file)
-    #     else:
-    #         logger.error('Req. file does not exist: %s' % req_file)
+    else:
+    # For debugging
+        req_file= debug_req_file
+        if os.path.isfile(req_file):
+            requests.create_archive_from_request(req_file)
+        else:
+            logger.error('Req. file does not exist: %s' % req_file)
