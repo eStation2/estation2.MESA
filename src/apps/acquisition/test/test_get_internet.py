1b07051def88cb833dae0a82d2d494d5b35bbddf
clerima <marco.clerici@jrc.ec.europa.eu>
2017-04-13 12:13:36 +0200

    Test ARC2-RFE

--- src/apps/acquisition/test/test_get_internet.py
+++ src/apps/acquisition/test/test_get_internet.py
@@ -666,8 +666,8 @@ class TestGetInternet(unittest.TestCase):
                          'pull_frequency': internet_source.pull_frequency,
                          'user_name':internet_source.user_name,
                          'password':internet_source.password,
-                         'start_date':20160101,
-                         'end_date':20160801,
+                         'start_date':-365,
+                         'end_date':-2,
                          'frequency_id': internet_source.frequency_id,
                          'type':internet_source.type}
 
