992bb6409825eda99962060eae1ba9453afc1419
clerima <marco.clerici@jrc.ec.europa.eu>
2017-08-24 17:20:11 +0200

    New Tests

--- src/apps/productmanagement/test/test_requests.py
+++ src/apps/productmanagement/test/test_requests.py
@@ -11,6 +11,7 @@ from __future__ import absolute_import
 import unittest
 import json
 import pprint
+import datetime as dt
 
 from apps.productmanagement import requests
 from apps.productmanagement import products
@@ -18,28 +19,27 @@ from apps.productmanagement import products
 from config import es_constants
 from lib.python import es_logging as log
 from apps.productmanagement import requests
+from lib.python import functions
 
 logger = log.my_logger(__name__)
 
 req_dir=es_constants.es2globals['requests_dir']
 
-class TestRequests(unittest.TestCase):
+class TestCreateRequests(unittest.TestCase):
 
+    # Type 1: only product/version defined
     def test_requests_1(self):
 
         test_json_dump = req_dir+'dump_my_json_1.req'
         mapsetcode = None
         subproductcode = None
-        # productcode = 'vgt-ndvi'
-        # version = 'sv2-pv2.1'
-        # # version = 'spot-v1'
-        # mapsetcode = 'SPOTV-Africa-1km'
-        # subproductcode = 'ndv'
 
-        productcode = 'lsasaf-lst'
-        version = 'undefined'
-        # version = 'spot-v1'
-        # mapsetcode = 'SPOTV-CEMAC-1km'
+        # The case below FAILS !! -> to be double-checked
+        # productcode = 'lsasaf-lst'
+        # version = 'undefined'
+
+        productcode = 'vgt-ndvi'
+        version = 'spot-v1'
 
         request = requests.create_request(productcode, version, mapsetcode=mapsetcode, subproductcode=subproductcode)
         request_json = json.dumps(request,
@@ -54,12 +54,13 @@ class TestRequests(unittest.TestCase):
 
         print test_json_dump
 
+    # Type 2: product/version/mapset defined
     def test_requests_2(self):
 
         req_dir=es_constants.es2globals['requests_dir']
         test_json_dump=req_dir+'/dump_my_json_2.req'
         productcode = 'vgt-ndvi'
-        version = 'sv2-pv2.1'
+        # version = 'sv2-pv2.1'
         version = 'spot-v2'
         mapsetcode = 'SPOTV-Africa-1km'
         subproductcode = None
@@ -76,15 +77,15 @@ class TestRequests(unittest.TestCase):
 
         print request_json
 
+    # Type 3: product/version/mapset/subproduct defined
     def test_requests_3(self):
 
         req_dir=es_constants.es2globals['requests_dir']
         test_json_dump=req_dir+'/dump_my_json_2.req'
-        productcode = 'lsasaf-lst'
-        version = 'lst'
-        version = 'undefined'
-        mapsetcode = None
-        subproductcode = None
+        productcode = 'vgt-ndvi'
+        version = 'spot-v1'
+        mapsetcode = 'SPOTV-Africa-1km'
+        subproductcode = 'ndv'
         request = requests.create_request(productcode, version, mapsetcode=mapsetcode, subproductcode=subproductcode)
         request_json = json.dumps(request,
                                ensure_ascii=False,
@@ -98,10 +99,104 @@ class TestRequests(unittest.TestCase):
 
         print request_json
 
-    def test_achive_creation_1(self):
+class TestCreateArchives(unittest.TestCase):
 
-        # Read the request - type c.
-        file_req='/eStation2/requests/fewsnet-rfe_2.0_FEWSNET-Africa-8km_all_enabled_datasets_2016-04-07_1213.req'
-        requests.create_archive_from_request(file_req)
+    # Create a .bsx archive by using the method: create_archive_vars()
+    # It does not go through the 'request' creation.
+
+    def test_create_archive_vars(self):
+
+        #   Creates and archive for a single product/sproduct/mapset/period
+        productcode='vgt-ndvi'
+        subproductcode = 'ndv'
+        version = 'sv2-pv2.2'
+        mapsetcode = 'SPOTV-ECOWAS-1km'
+        from_date='2011-01-01'
+        to_date='2011-12-31'
+        output_dir='/eStation2/archives/bsx/'
+
+        bsx_archive = requests.create_archive_vars(productcode, version, mapsetcode, subproductcode, from_date, to_date,
+                                               output_dir=output_dir)
+
+        logger.info('Archive created as {0}'.format(bsx_archive))
+
         return
 
+    # Create a .bsx archives for NDVI 2.2
+    def test_achive_creation_ndvi_2_2(self):
+
+        #   General definitions
+        productcode='vgt-ndvi'
+        version = 'sv2-pv2.2'
+        output_dir='/data/bsx_archives/'
+
+        #   ================================================================
+        #   10d linearx2 LTA
+        #   ================================================================
+
+        subproductcodes = ('10davg-linearx2','10dmin-linearx2','10dmax-linearx2','10dmed-linearx2')
+        regions=('IGAD','ECOWAS','SADC','CEMAC')
+        from_date = None
+        to_date = None
+        for subproductcode in subproductcodes:
+            for region in regions:
+
+                mapsetcode='SPOTV-{0}-1km'.format(region)
+                output_subdir='/data/bsx_archives/{0}'.format(region)
+
+                bsx_archive = requests.create_archive_vars(productcode, version, mapsetcode, subproductcode,
+                                                           from_date=from_date, to_date=to_date, time_suffix='36-dekads', output_dir=output_subdir)
+
+                logger.info('Archive created as {0}'.format(bsx_archive))
+
+
+
+    # Create a .bsx archives for MODIS-SST (for subsetting to IOC region)
+    def test_achive_creation_modis_sst(self):
+
+        #   General definitions
+        productcode='modis-sst'
+        version = 'v2013.1'
+        output_dir='/data/bsx_archives/'
+
+        #   ================================================================
+        #
+        #   ================================================================
+
+        subproductcodes = ('monavg','monclim','monanom')
+        from_date = '2010-01-01'
+        to_date = '2017-07-01'
+        for subproductcode in subproductcodes:
+
+                mapsetcode='MODIS-IOC-4km'
+                output_subdir='/data/bsx_archives/{0}'.format(mapsetcode)
+
+                bsx_archive = requests.create_archive_vars(productcode, version, mapsetcode, subproductcode,
+                                                           from_date=from_date, to_date=to_date, time_suffix='2010-2017', output_dir=output_subdir)
+
+                logger.info('Archive created as {0}'.format(bsx_archive))
+
+    # Create a .bsx archives for MODIS-CHLA (for subsetting to IOC region)
+    def test_achive_creation_modis_chla(self):
+
+        #   General definitions
+        productcode='modis-chla'
+        version = 'v2013.1'
+        output_dir='/data/bsx_archives/'
+
+        #   ================================================================
+        #
+        #   ================================================================
+
+        subproductcodes = ('monavg','monclim','monanom')
+        from_date = '2010-01-01'
+        to_date = '2017-07-01'
+        for subproductcode in subproductcodes:
+
+                mapsetcode='MODIS-IOC-4km'
+                output_subdir='/data/bsx_archives/{0}'.format(mapsetcode)
+
+                bsx_archive = requests.create_archive_vars(productcode, version, mapsetcode, subproductcode,
+                                                           from_date=from_date, to_date=to_date, time_suffix='2010-2017', output_dir=output_subdir)
+
+                logger.info('Archive created as {0}'.format(bsx_archive))
