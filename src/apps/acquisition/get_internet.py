71d91b184b056090f753ac3bd82717aa2c5006de
clerima <marco.clerici@jrc.ec.europa.eu>
2018-03-19 17:51:18 +0100

    Re-activate check internet_on()

--- src/apps/acquisition/get_internet.py
+++ src/apps/acquisition/get_internet.py
@@ -385,7 +385,7 @@ def loop_get_internet(dry_run=False):
         while 1:
 
             # Check internet connection (or continue)
-            if not True:        #functions.internet_on():
+            if not functions.internet_on():
                 logger.error("The computer is not currently connected to the internet. Wait 1 minute.")
                 time.sleep(60)
 
