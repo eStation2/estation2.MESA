dee96d1954516d8432c6bdee8315eb4279e0365b
venkavi <Vijay.VENKATACHALAM@ext.ec.europa.eu>
2018-11-20 14:45:12 +0100

    spirits data conversion new method

--- src/apps/es2system/convert_2_spirits.py
+++ src/apps/es2system/convert_2_spirits.py
@@ -12,13 +12,15 @@ import operator
 from config import es_constants
 import fnmatch
 import datetime
-
+from osgeo import gdal, osr
 # Import eStation2 modules
 from lib.python import es_logging as log
 from lib.python import functions
 from database import querydb
 from apps.productmanagement.datasets import Dataset
 from apps.productmanagement.products import Product
+from apps.acquisition.ingestion import rescale_data, conv_data_type_to_gdal, conv_data_type_to_numpy
+from lib.python import metadata
 
 logger = log.my_logger(__name__)
 
@@ -92,6 +94,7 @@ def convert_geotiff_file(input_file, output_dir, str_date, naming_spirits, metad
     # Check output file exist
     if not os.path.isfile(output_path) and not overwrite:
         command = es_constants.es2globals['gdal_translate']+ \
+                  ' -ot Byte ' + \
                   ' -of ENVI ' + \
                   input_file  + ' ' + \
                   output_path
@@ -108,10 +111,74 @@ def convert_geotiff_file(input_file, output_dir, str_date, naming_spirits, metad
     if status:
          logger.error('Error in modifying SPIRITS header: %s' % header_file_name)
 
+
+def convert_geotiff_datatype_rescaled(input_file, output_dir, str_date, naming_spirits, metadata_spirits,
+                                      in_scale_factor, in_offset, in_nodata, out_scale_factor, out_offset, out_nodata,
+                                      out_data_type=None, mask_min=None, mask_max=None, overwrite=False):
+
+
+    f1Fid = gdal.Open(input_file, gdal.GA_ReadOnly)
+    nb = f1Fid.RasterCount
+    orig_size_x = f1Fid.RasterXSize
+    orig_size_y = f1Fid.RasterYSize
+    dataType = f1Fid.GetRasterBand(1).DataType
+    geoTransform = f1Fid.GetGeoTransform()
+    projection = f1Fid.GetProjection()
+    driver_type = f1Fid.GetDriver().ShortName
+
+    # Translate data type for gdal and numpy
+    out_data_type_gdal = conv_data_type_to_gdal(out_data_type)
+    out_data_type_numpy = conv_data_type_to_numpy(out_data_type)
+
+    # Read from input file
+    band = f1Fid.GetRasterBand(1)
+    #my_logger.debug('Band Type=' + gdal.GetDataTypeName(band.DataType))
+    in_data = band.ReadAsArray(0, 0, orig_size_x, orig_size_y)
+    # rescale the data
+    # Apply rescale to data
+    scaled_data = rescale_data(in_data, in_scale_factor,in_offset, in_nodata, mask_min,
+                               mask_max,
+                               out_data_type_numpy, out_scale_factor, out_offset, out_nodata,
+                               logger,
+                               None)
+
+
+    extension_bin = '.img'
+    extension_hdr = '.hdr'
+    status = 0
+    # Define output filename
+    output_base_name = naming_spirits['sensor_filename_prefix']+\
+                  naming_spirits['frequency_filename_prefix']+\
+                  str_date+ \
+                  naming_spirits['pa_filename_prefix']
+
+    output_file = output_dir+os.path.sep+output_base_name+extension_bin
+    #outType = conv_data_type_to_gdal('Byte')
+    out_driver = gdal.GetDriverByName('ENVI')
+    out_driver = out_driver.Create(output_file, orig_size_x, orig_size_y, nb, out_data_type_gdal)
+    out_driver.SetGeoTransform(geoTransform)
+    out_driver.SetProjection(projection)
+    # Create a copy to output_file
+    # trg_ds = out_driver.CreateCopy(my_output_filename, out_ds, 0,
+    #                                [es_constants.ES2_OUTFILE_OPTIONS])
+    out_driver.GetRasterBand(1).WriteArray(scaled_data)
+
+    out_driver = None
+    # Modify the header
+    header_file_name = output_dir+os.path.sep+output_base_name+extension_hdr
+    status = append_to_header_file(header_file_name, metadata_spirits)
+
+    if status:
+         logger.error('Error in modifying SPIRITS header: %s' % header_file_name)
+
+
 def convert_driver(output_dir=None):
 
     # Definitions
     input_dir = es_constants.es2globals['processing_dir']
+
+    # Instance metadata object
+    sds_meta = metadata.SdsMetadata()
 
     # Check base output dir
     if output_dir is None:
@@ -126,7 +193,10 @@ def convert_driver(output_dir=None):
         product_code = entry['productcode']
         sub_product_code = entry['subproductcode']
         version = entry['version']
-        mapset = entry['mapsetcode']
+        out_data_type = entry['out_data_type']
+        out_scale_factor = entry['out_scale_factor']
+        out_offset = entry['out_offset']
+        out_nodata = entry['data_ignore_value']
 
         # Prepare the naming dict
         naming_spirits = { 'sensor_filename_prefix':entry['sensor_filename_prefix'], \
@@ -172,6 +242,18 @@ def convert_driver(output_dir=None):
 
             logger.info('Working on [%s]/[%s]/[%s]/[%s]' % (product_code,version,my_mapset,sub_product_code))
             ds = Dataset(product_code,sub_product_code,my_mapset,version=version,from_date=from_date,to_date=to_date)
+            product_info = ds._db_product
+            in_scale_factor = product_info.scale_factor
+            in_offset = product_info.scale_offset
+            in_nodata = product_info.nodata
+            mask_min = product_info.mask_min
+            mask_max = product_info.mask_max
+            productcode = product_info.productcode
+            subproductcode = product_info.subproductcode
+
+            if productcode == 'vgt-ndvi' and  subproductcode == 'ndv':
+                mask_min = 0
+
             if use_range:
                 available_files=ds.get_filenames_range()
             else:
@@ -195,7 +277,19 @@ def convert_driver(output_dir=None):
                             else:
                                 metadata_spirits['date'] = str_date
 
+                            #Read metadata from the file and differentiate chirps prelim and final data
+                            sds_meta.read_from_file(input_file)
+                            input_file_name = sds_meta.get_item('eStation2_input_files')
+                            if productcode == 'chirps-dekad' and input_file_name.endswith(".tif;"):
+                                metadata_spirits['comment'] = 'Prelim ' +entry['comment']
+                            elif productcode == 'chirps-dekad' and input_file_name.endswith(".gz;"):
+                                metadata_spirits['comment'] = 'Final ' +entry['comment']
+
                             # Check output file exists
-                            convert_geotiff_file(input_file, output_dir+out_sub_dir, str_date, naming_spirits, metadata_spirits)
+                            #convert_geotiff_file(input_file, output_dir+out_sub_dir, str_date, naming_spirits, metadata_spirits)
+                            convert_geotiff_datatype_rescaled(input_file, output_dir + out_sub_dir, str_date,
+                                                              naming_spirits, metadata_spirits, in_scale_factor,
+                                                              in_offset, in_nodata, out_scale_factor, out_offset,
+                                                              out_nodata, out_data_type, mask_min, mask_max)
                         else:
                             logger.debug('Input file does not exist: %s' % input_file)
