d1abc385414ca7b95b9a9893a0497a5cc6e76f8c
clerima <marco.clerici@jrc.ec.europa.eu>
2019-01-07 13:48:38 +0100

    Clean code

--- src/apps/tools/create_archive_from_request.py
+++ src/apps/tools/create_archive_from_request.py
@@ -82,33 +82,3 @@ if __name__=='__main__':
     # req_directory = '/data/processing/exchange/requests/Seychelles/'
     # create_archives_from_dir(req_directory)
 
-
-#
-#     debug = 0
-#     req_file = None
-#     req_directory = '/data/processing/exchange/requests/20170414_Ref_Station_JRC/'
-#     debug_req_file = None
-#
-#     if not debug:
-#     # Parse the input
-#     #     parser = argparse.ArgumentParser(description='Create bsx archive from request')
-#     #     parser.add_argument('req_file', type=str, help='Name of the request file/dir to process')
-#
-#         # Commented FTTB: to be implemented through a GUI
-#         # parser.add_argument('--file', type=str, help='The passed arg is full filename')
-#         # parser.add_argument('--directory', type=str, help='The passed arg is directory name')
-#         #
-#         # args = parser.parse_args()
-#         # req_file= args.req_file
-#
-#
-#         # Commented FTTB: to be implemented through a GUI
-#             # Get list of all req. in the dir
-#     else:
-#     # For debugging
-#         req_file= debug_req_file
-#         if os.path.isfile(req_file):
-#             requests.create_archive_from_request(req_file)
-#         else:
-#             logger.error('Req. file does not exist: %s' % req_file)
-#
