e7401ac22021f14afe37c4812c77f3f1a06e6596
clerima <marco.clerici@jrc.ec.europa.eu>
2020-02-26 14:33:13 +0100

    Updated for ES2-502

--- src/apps/productmanagement/helpers.py
+++ src/apps/productmanagement/helpers.py
@@ -84,11 +84,20 @@ def add_cgl_dekads(date, dekads=1):
     # Copernicus global data products are obtained from the vito website and the dekad naming format is different from the actual dekad patern.
     # The dekadal dates are 1stDekad - 10th day, 2ndDekad - 20th day and 3rdDekad - last day of the month.
     new_date = date
+    #ES2-502
+    isdatetime = isinstance(date, datetime.datetime)
     # ES2-281 Robust way to handle the dates
     if date.day >= 1 and date.day < 10:
-        new_date = datetime.datetime(new_date.year, new_date.month, 10)
+        # ES2-502
+        if isdatetime:
+            new_date = datetime.datetime(new_date.year, new_date.month, 10)
+        else:
+            new_date = datetime.date(new_date.year, new_date.month, 10)
     elif date.day >= 10 and date.day < 20:
-        new_date = datetime.datetime(new_date.year, new_date.month, 20)
+        if isdatetime:
+            new_date = datetime.datetime(new_date.year, new_date.month, 20)
+        else:
+            new_date = datetime.date(new_date.year, new_date.month, 20)
     # elif date.day >= 20 and date.day < 27:
     #     tot_days = functions.get_number_days_month(str(date.year)+date.strftime('%m')+date.strftime('%d'))
     #     new_date = datetime.datetime(new_date.year, new_date.month, tot_days)
@@ -96,11 +105,17 @@ def add_cgl_dekads(date, dekads=1):
         tot_days = functions.get_number_days_month(str(date.year) + date.strftime('%m') + date.strftime('%d'))  #Last day of the month
         # check if the current day is last date of the month
         if date.day != tot_days:
-            new_date = datetime.datetime(new_date.year, new_date.month, tot_days)
+            if isdatetime:  #ES2-502
+                new_date = datetime.datetime(new_date.year, new_date.month, tot_days)
+            else:
+                new_date = datetime.date(new_date.year, new_date.month, tot_days)
         else:
             new_date = add_months(date, 1)
             if new_date.month != date.month:
-                new_date = datetime.datetime(new_date.year, new_date.month, 10)
+                if isdatetime: #ES2-502
+                    new_date = datetime.datetime(new_date.year, new_date.month, 10)
+                else:
+                    new_date = datetime.date(new_date.year, new_date.month, 10)
     date = new_date
     return date
 
