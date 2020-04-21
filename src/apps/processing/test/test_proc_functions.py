fdededc1491eca67316794f29fde1f09152f9b26
jurvtk <jurvtk@gmail.com>
2020-04-21 17:22:41 +0200

    test_proc_functions.py

--- src/apps/processing/test/test_proc_functions.py
+++ src/apps/processing/test/test_proc_functions.py
@@ -9,9 +9,12 @@ __author__ = "Marco Clerici"
 
 from unittest import TestCase
 import os
+import glob
 from lib.python import es_logging as log
 from lib.python import functions
 from config import es_constants
+from lib.python.image_proc import raster_image_math
+from lib.python import metadata as md
 # Trivial change
 logger = log.my_logger(__name__)
 
@@ -19,11 +22,72 @@ from apps.processing import proc_functions
 
 class TestProcFunctions(TestCase):
 
-    def Test_reproject_output(self):
+    def setUp(self):
+        root_test_dir=es_constants.es2globals['test_data_dir']
+        self.test_procfunc_dir=root_test_dir #os.path.join(root_test_dir,'native')
+        self.proc_dir_bck = es_constants.processing_dir
+        es_constants.processing_dir = es_constants.es2globals['base_tmp_dir']+os.path.sep
+        self.ingest_out_dir = es_constants.processing_dir
+        self.ref_out_dir = root_test_dir #os.path.join(root_test_dir,'refs_output')
 
-        input_file = '/data/processing/lsasaf-et/undefined/MSG-satellite-3km/derived/10d30min/201510010000_lsasaf-et_10d30min_MSG-satellite-3km_undefined.tif'
-        # input_file = '/data/processing/lsasaf-lst/undefined/MSG-satellite-3km/tif/lst/201510011230_lsasaf-lst_lst_MSG-satellite-3km_undefined.tif'
-        # input_file = '/data/processing/fewsnet-rfe/2.0/FEWSNET-Africa-8km/tif/10d/20010221_fewsnet-rfe_10d_FEWSNET-Africa-8km_2.0.tif'
+    def tearDown(self):
+        es_constants.processing_dir = self.proc_dir_bck
+
+    def checkFile(self, productcode='', subproductcode='', version='', mapsetcode='', date=''):
+        # Given the all files keys (date, prod, sprod, ...) finds out:
+        # -> the product just ingested in the tmp_dir (see setUp)
+        # -> the product in refs_output
+        # Assess if the products are equal/equivalent
+
+        result = 0
+        filename = functions.set_path_filename(date,productcode, subproductcode, mapsetcode, version, '.tif')
+        sub_directory= functions.set_path_sub_directory(productcode,subproductcode,'Ingest',version,mapsetcode)
+
+        ref_file = glob.glob(self.ref_out_dir+'**/*/*/'+filename, recursive=True)
+        if not len(ref_file)>0: #os.path.isfile(ref_file[0]):
+            print("Error reference file does not exist: "+filename)
+            return result
+        newly_computed_file = glob.glob(self.ingest_out_dir+sub_directory+filename,recursive=True)
+        if not len(newly_computed_file)>0: #os.path.isfile(newly_computed_file[0]):
+            print("Error reference file does not exist: "+filename)
+            return result
+
+        # Compare the files by using gdal_info objects
+        if len(ref_file)>0 and len(newly_computed_file)>0 and os.path.exists(ref_file[0]) and os.path.exists(newly_computed_file[0]):
+            gdal_info_ref = md.GdalInfo()
+            gdal_info_ref.get_gdalinfo(ref_file[0])
+            gdal_info_new = md.GdalInfo()
+            gdal_info_new.get_gdalinfo(newly_computed_file[0])
+            equal = gdal_info_new.compare_gdalinfo(gdal_info_ref)
+
+            if not equal:
+                print("Warning: the files metadata are different")
+            # Check the raster array compare
+            array_equal = raster_image_math.compare_two_raster_array(ref_file[0], newly_computed_file[0])
+            if not array_equal:
+                print("Warning: the files contents are different")
+
+            if array_equal is True:
+                result = 1
+
+        return result
+
+    def test_get_list_dates_for_dataset(self):
+        productcode = 'lsasaf-et'
+        productversion = 'undefined'
+        subproductcode = '10d30min'
+        input_file = self.test_procfunc_dir+os.path.sep+productcode+os.path.sep+subproductcode+os.path.sep+'202004010000_lsasaf-et_10d30min_MSG-satellite-3km_undefined.tif'
+        start_date = 202004010000
+        end_date = 202004010120
+        proc_functions.get_list_dates_for_dataset(productcode, subproductcode, productversion,start_date, end_date)
+
+        self.assertEqual(1, 1)
+
+
+    def test_reproject_output(self):
+        productcode = 'lsasaf-et'
+        subproductcode = '10d30min'
+        input_file = self.test_procfunc_dir+os.path.sep+productcode+os.path.sep+subproductcode+os.path.sep+'202004010000_lsasaf-et_10d30min_MSG-satellite-3km_undefined.tif'
 
         target_mapset_id = 'SPOTV-ECOWAS-1km'
         orig_mapset_id = 'MSG-satellite-3km'
