f78068360e4f71749373a4147939452a845d3f09
jurvtk <jurvtk@gmail.com>
2020-04-30 17:20:37 +0200

    Incomplete proc function

--- src/apps/processing/test/test_proc_functions.py
+++ src/apps/processing/test/test_proc_functions.py
@@ -33,44 +33,44 @@ class TestProcFunctions(TestCase):
     def tearDown(self):
         es_constants.processing_dir = self.proc_dir_bck
 
-    def checkFile(self, productcode='', subproductcode='', version='', mapsetcode='', date=''):
-        # Given the all files keys (date, prod, sprod, ...) finds out:
-        # -> the product just ingested in the tmp_dir (see setUp)
-        # -> the product in refs_output
-        # Assess if the products are equal/equivalent
-
-        result = 0
-        filename = functions.set_path_filename(date,productcode, subproductcode, mapsetcode, version, '.tif')
-        sub_directory= functions.set_path_sub_directory(productcode,subproductcode,'Ingest',version,mapsetcode)
-
-        ref_file = glob.glob(self.ref_out_dir+'**/*/*/'+filename, recursive=True)
-        if not len(ref_file)>0: #os.path.isfile(ref_file[0]):
-            print("Error reference file does not exist: "+filename)
-            return result
-        newly_computed_file = glob.glob(self.ingest_out_dir+sub_directory+filename,recursive=True)
-        if not len(newly_computed_file)>0: #os.path.isfile(newly_computed_file[0]):
-            print("Error reference file does not exist: "+filename)
-            return result
-
-        # Compare the files by using gdal_info objects
-        if len(ref_file)>0 and len(newly_computed_file)>0 and os.path.exists(ref_file[0]) and os.path.exists(newly_computed_file[0]):
-            gdal_info_ref = md.GdalInfo()
-            gdal_info_ref.get_gdalinfo(ref_file[0])
-            gdal_info_new = md.GdalInfo()
-            gdal_info_new.get_gdalinfo(newly_computed_file[0])
-            equal = gdal_info_new.compare_gdalinfo(gdal_info_ref)
-
-            if not equal:
-                print("Warning: the files metadata are different")
-            # Check the raster array compare
-            array_equal = raster_image_math.compare_two_raster_array(ref_file[0], newly_computed_file[0])
-            if not array_equal:
-                print("Warning: the files contents are different")
-
-            if array_equal is True:
-                result = 1
-
-        return result
+    # def checkFile(self, productcode='', subproductcode='', version='', mapsetcode='', date=''):
+    #     # Given the all files keys (date, prod, sprod, ...) finds out:
+    #     # -> the product just ingested in the tmp_dir (see setUp)
+    #     # -> the product in refs_output
+    #     # Assess if the products are equal/equivalent
+    #
+    #     result = 0
+    #     filename = functions.set_path_filename(date,productcode, subproductcode, mapsetcode, version, '.tif')
+    #     sub_directory= functions.set_path_sub_directory(productcode,subproductcode,'Ingest',version,mapsetcode)
+    #
+    #     ref_file = glob.glob(self.ref_out_dir+'**/*/*/'+filename, recursive=True)
+    #     if not len(ref_file)>0: #os.path.isfile(ref_file[0]):
+    #         print("Error reference file does not exist: "+filename)
+    #         return result
+    #     newly_computed_file = glob.glob(self.ingest_out_dir+sub_directory+filename,recursive=True)
+    #     if not len(newly_computed_file)>0: #os.path.isfile(newly_computed_file[0]):
+    #         print("Error reference file does not exist: "+filename)
+    #         return result
+    #
+    #     # Compare the files by using gdal_info objects
+    #     if len(ref_file)>0 and len(newly_computed_file)>0 and os.path.exists(ref_file[0]) and os.path.exists(newly_computed_file[0]):
+    #         gdal_info_ref = md.GdalInfo()
+    #         gdal_info_ref.get_gdalinfo(ref_file[0])
+    #         gdal_info_new = md.GdalInfo()
+    #         gdal_info_new.get_gdalinfo(newly_computed_file[0])
+    #         equal = gdal_info_new.compare_gdalinfo(gdal_info_ref)
+    #
+    #         if not equal:
+    #             print("Warning: the files metadata are different")
+    #         # Check the raster array compare
+    #         array_equal = raster_image_math.compare_two_raster_array(ref_file[0], newly_computed_file[0])
+    #         if not array_equal:
+    #             print("Warning: the files contents are different")
+    #
+    #         if array_equal is True:
+    #             result = 1
+    #
+    #     return result
 
     def test_get_list_dates_for_dataset(self):
         productcode = 'lsasaf-et'
@@ -84,20 +84,20 @@ class TestProcFunctions(TestCase):
         self.assertEqual(1, 1)
 
 
-    def test_reproject_output(self):
-        productcode = 'lsasaf-et'
-        subproductcode = '10d30min'
-        input_file = self.test_procfunc_dir+os.path.sep+productcode+os.path.sep+subproductcode+os.path.sep+'202004010000_lsasaf-et_10d30min_MSG-satellite-3km_undefined.tif'
+    # def test_reproject_output(self):
+    #     productcode = 'lsasaf-et'
+    #     subproductcode = '10d30min'
+    #     input_file = self.test_procfunc_dir+os.path.sep+productcode+os.path.sep+subproductcode+os.path.sep+'202004010000_lsasaf-et_10d30min_MSG-satellite-3km_undefined.tif'
+    #
+    #     target_mapset_id = 'SPOTV-ECOWAS-1km'
+    #     orig_mapset_id = 'MSG-satellite-3km'
+    #     # orig_mapset_id = 'FEWSNET-Africa-8km'
+    #
+    #     proc_functions.reproject_output(input_file, orig_mapset_id, target_mapset_id, logger)
+    #
+    #     self.assertEqual(1, 1)
 
-        target_mapset_id = 'SPOTV-ECOWAS-1km'
-        orig_mapset_id = 'MSG-satellite-3km'
-        # orig_mapset_id = 'FEWSNET-Africa-8km'
-
-        proc_functions.reproject_output(input_file, orig_mapset_id, target_mapset_id, logger)
-
-        self.assertEqual(1, 1)
-
-    def Test_remove_old_files(self):
+    def test_remove_old_files(self):
 
         productcode='lsasaf-lst'
         subprod='lst'
