134ce961f8641148e65d4c607df3eed7c361ebcd
jurvtk <jurvtk@gmail.com>
2020-05-15 12:01:14 +0200

    setup_estationdb.py

--- build-ubuntu/scripts/setup_estationdb.py
+++ build-ubuntu/scripts/setup_estationdb.py
@@ -1,23 +1,27 @@
 import psycopg2
 
-DB_HOST = ""
-DB_PORT = 5432
-DB_NAME = ""
-RW_USER = ""
+DB_HOST = "jeodb01.cidsn.jrc.it"
+DB_PORT = 65431
+DB_NAME = "estationdb"
+RW_USER = "estation"
 RW_PASS = ""
+FILE_DUMP_STRUCTURE="/var/www/eStation2/database/dbInstall/products_dump_structure_only.sql"
 
 DSN = f"postgresql://{RW_USER}:{RW_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 conn = psycopg2.connect(DSN)
 curs = conn.cursor()
 
-SQL = """
-CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);
-INSERT INTO test
-            (num, data)
-     VALUES (121, 'aaa')
-          , (212, 'bbb')
-          , (313, 'ccc')
-          ;
-"""
-curs.execute(SQL)
+# SQL = """
+# CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);
+# INSERT INTO test
+#             (num, data)
+#      VALUES (121, 'aaa')
+#           , (212, 'bbb')
+#           , (313, 'ccc')
+#           ;
+# """
+
+for line in open(FILE_DUMP_STRUCTURE):
+    curs.execute(line)
+# curs.execute(SQL)
