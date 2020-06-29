2c8ac80d467ab58d1e4edc1de09de908b3994979
Marco <clerici.marco@gmail.com>
2020-06-29 10:26:34 +0200

    Modify to test few basics things (see ES2-501)

--- src/apps/acquisition/test/test_get_eumetcast.py
+++ src/apps/acquisition/test/test_get_eumetcast.py
@@ -12,67 +12,41 @@ import io
 import io
 from apps.acquisition.get_eumetcast import *
 from database import querydb
-
+import glob
 #
 class TestGetEumetcast(unittest.TestCase):
-#
-#     #   ---------------------------------------------------------------------------
-#     #   Test get_eumetcast_info()
-#     #   ---------------------------------------------------------------------------
-#     def TestGetEumetcastInfo(self):
-#         db = querydb.db
-#         data_acquisitions = querydb.get_dataacquisitions(toJSON=False)
-#
-#         for row in data_acquisitions:
-#             print row.data_source_id
-#             # Retrieve datetime of latest acquired file and lastest datetime
-#             # the acquisition was active of a specific eumetcast id
-#             acq_dates = get_eumetcast.get_eumetcast_info(row.data_source_id)
-#             if acq_dates:
-#                 for key in acq_dates.keys():
-#                     print "key: %s , value: %s" % (key, acq_dates[key])
-#                 #print "time_latest_copy: "+acq_dates['time_latest_copy']
-#                 #print "time_latest_exec: "+acq_dates['time_latest_exec']
-#             else:
-#                 print "Datasource " + row.data_source_id + " has not been activated!"
-#             #logger.info('Datetime latest file: '+acq_dates)
-#             #logger.info('Datetime last active: '+acq_dates)
-#
-#         self.assertEqual(1, 1)
 
-    def test_GetEumetcats_PC2_nodir(self):
+    # Check the eumetcast dir exists, is accessible and not empty
+    def test_GetEumetcast_dir(self):
 
-        remote_url='ftp://mesa-pc1//space/efts/fromTellicast/forEstation/'
-        usr_pwd='mesadata:mesadata'
-        d = pycurl.Curl()
-        response = io.StringIO()
-        d.setopt(pycurl.URL, remote_url)
-        d.setopt(pycurl.USERPWD, usr_pwd)
-        d.setopt(pycurl.FOLLOWLOCATION, 1)
-        d.setopt(pycurl.WRITEFUNCTION, response.write)
-        d.perform()
-        d.close()
-        current_list = []
-        content=response.getvalue()
-        lines = content.split('\n')
-        for line in lines:
-            check_line = len(str(line))
-            if check_line is not 0:
-                line_dir=line.split()[-1]
-                current_list.append(line_dir)
-        print (current_list)
-        return current_list
+        error_in_reading_vars = False
+        try:
+            input_dir = es_constants.eumetcast_files_dir
+            output_dir = es_constants.get_eumetcast_output_dir
+            user_def_sleep = es_constants.get_eumetcast_sleep_time_sec
+        except:
+            error_in_reading_vars = True
 
+        dir_exists = os.path.exists(input_dir)
+        list_files = glob.glob(input_dir+'*')
+        files_exists = len(list_files)
 
-    def test_GetEumetcats_PC2_homedir(self):
-        filter_expression_jrc='/data/processing/*'
-        ftp_eumetcast_url='ftp://mesa-pc2'
-        ftp_eumetcast_userpwd='root:rootroot'
-        current_list = get_list_matching_files_dir_ftp(ftp_eumetcast_url, ftp_eumetcast_userpwd, filter_expression_jrc)
+        self.assertEqual(error_in_reading_vars, False)
+        self.assertEqual(dir_exists, True)
+        self.assertGreater(files_exists,0)
 
+    # Check reading sources from DB is ok
+    def test_GetEumetcast_sources(self):
 
-    def test_GetEumetcast_Archives(self):
+        error_in_db = False
+        try:
+            eumetcast_sources_list = querydb.get_eumetcast_sources()
+        except:
+            error_in_db = True
 
-        print ('Start Test')
-        get_archives_eumetcast()
-        print ('Test Finished')
+        self.assertEqual(error_in_db, False)
+
+suite_get_eumetcast = unittest.TestLoader().loadTestsFromTestCase(TestGetEumetcast)
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite_get_eumetcast)
