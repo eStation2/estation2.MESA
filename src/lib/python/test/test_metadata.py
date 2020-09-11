f0261ae70c8836b78c5492355cb1e37d9abd8a10
clerima <marco.clerici@jrc.ec.europa.eu>
2020-09-11 16:01:50 +0200

    Updated to latest testing - python3_docker branch (see ES2-596)

--- src/lib/python/test/test_metadata.py
+++ src/lib/python/test/test_metadata.py
@@ -13,13 +13,12 @@ import tempfile
 import shutil
 from config import es_constants
 
-#input_dir = es_constants.es2globals['test_data_refs_dir']+'Metadata/'
-# Put here an existing 2.0 file with correct metadata
-input_dir = es_constants.es2globals['data_dir']+'/data/processing/vgt_ndvi/WGS84_Africa_1km/tif/ndv/'
-file=input_dir+'20130701_vgt_ndvi_ndv_WGS84_Africa_1km.tif'
-file_eStation2='/data/processing/fewsnet-rfe/2.0/FEWSNET-Africa-8km/tif/10d/20110111_fewsnet-rfe_10d_FEWSNET-Africa-8km_2.0.tif'
 
-class TestMapSet(TestCase):
+class TestMetaData(TestCase):
+
+    # Put here an existing 2.0 file with correct metadata - nodata = -32768
+    root_test_dir = es_constants.es2globals['test_data_dir']
+    test_file = root_test_dir + os.path.sep + 'tamsat-rfe/10d/20200121_tamsat-rfe_10d_TAMSAT-Africa-4km_3.0.tif'
 
     def create_temp_file(self):
         tf = tempfile.NamedTemporaryFile()
@@ -31,14 +30,6 @@ class TestMapSet(TestCase):
 
         my_item='Test_Metadata_Item'
         my_value='Test_Metadata_Value'
-
-        # Create a tmp Tiff file
-        #try:
-        #    tmpdir = tempfile.mkdtemp(prefix=__name__, suffix='_test_writing_an_item', dir=locals.es2globals['temp_dir'])
-        #except IOError:
-        #    logger.error('Cannot create temporary dir ' + es_constants.es2globals['temp_dir'] + '. Exit')
-        #    return 1
-        #filename = tmpdir+'/temp_target.tif'
 
         filename = self.create_temp_file()
 
@@ -71,6 +62,8 @@ class TestMapSet(TestCase):
         out_ds = None
         gtiff_driver = None
 
+        self.assertTrue(os.path.isfile(filename))
+
     def test_writing_meta_to_existing_file(self):
 
         sds_meta = SdsMetadata()
@@ -90,24 +83,12 @@ class TestMapSet(TestCase):
 
         self.assertTrue(os.path.isfile(filename))
 
-    def test_writing_meta_to_estation2_file(self):
-
-        sds_meta = SdsMetadata()
-
-        # Create a dummy output File
-
-        sds_meta.write_to_file(file_eStation2)
-
-        self.assertTrue(os.path.isfile(file_eStation2))
-
     def test_reading_nodata_value(self):
 
-        myfile='/data/processing//vgt-ndvi/sv2-pv2.1/SPOTV-Africa-1km/derived/ndvi_linearx2/20000811_vgt-ndvi_ndvi_linearx2_SPOTV-Africa-1km_sv2-pv2.1.tif'
-
         sds_meta = SdsMetadata()
 
-        if os.path.exists(myfile):
-            nodata=sds_meta.get_nodata_value(myfile)
+        if os.path.exists(self.test_file):
+            nodata=sds_meta.get_nodata_value(self.test_file)
             self.assertEqual(float(nodata), -32768)
 
         else:
@@ -115,95 +96,36 @@ class TestMapSet(TestCase):
 
     def test_reading_meta_items_from_file(self):
 
-        if os.path.exists(file):
+        if os.path.exists(self.test_file):
              sds_meta = SdsMetadata()
              # Read from a reference file
-             sds_meta.read_from_file(file)
+             sds_meta.read_from_file(self.test_file)
              sds_meta.print_out()
 
              value = sds_meta.get_item('eStation2_mapset')
-             self.assertEqual(value, 'FEWSNET_Africa_8km')
+             self.assertEqual(value, 'TAMSAT-Africa-4km')
 
              value = sds_meta.get_item('eStation2_nodata')
              self.assertEqual(value, '-32768')
-
-             value = sds_meta.get_item('eStation2_es2_version')
-             self.assertEqual(value, 'my_eStation2_sw_release')
 
              value = sds_meta.get_item('eStation2_conversion')
              self.assertEqual(value, 'Phys = DN * scaling_factor + scaling_offset')
 
              value = sds_meta.get_item('eStation2_input_files')
-             self.assertEqual(value, '/data/Archives/FewsNET/a14061rb.zip;')
+             self.assertEqual(value, 'rfe2020_01-dk3.v3.nc;')
 
              value = sds_meta.get_item('eStation2_subProduct')
-             self.assertEqual(value, 'rfe')
+             self.assertEqual(value, '10d')
 
              value = sds_meta.get_item('eStation2_product')
-             self.assertEqual(value, 'fewsnet_rfe')
+             self.assertEqual(value, 'tamsat-rfe')
 
              value = sds_meta.get_item('eStation2_scaling_factor')
              self.assertEqual(value, '1.0')
 
              value = sds_meta.get_item('eStation2_unit')
-             self.assertEqual(value, None)
+             self.assertEqual(value, 'mm')
 
         else:
             logger.info('Test file not existing: skip test')
 
-    def test_reading_meta_items_from_file(self):
-
-        if os.path.exists(file):
-             sds_meta = SdsMetadata()
-             # Read from a reference file
-             sds_meta.read_from_file(file)
-             sds_meta.print_out()
-
-             value = sds_meta.get_item('eStation2_mapset')
-             self.assertEqual(value, 'FEWSNET_Africa_8km')
-
-             value = sds_meta.get_item('eStation2_nodata')
-             self.assertEqual(value, '-32768')
-
-             value = sds_meta.get_item('eStation2_es2_version')
-             self.assertEqual(value, 'my_eStation2_sw_release')
-
-             value = sds_meta.get_item('eStation2_conversion')
-             self.assertEqual(value, 'Phys = DN * scaling_factor + scaling_offset')
-
-             value = sds_meta.get_item('eStation2_input_files')
-             self.assertEqual(value, '/data/Archives/FewsNET/a14061rb.zip;')
-
-             value = sds_meta.get_item('eStation2_subProduct')
-             self.assertEqual(value, 'rfe')
-
-             value = sds_meta.get_item('eStation2_product')
-             self.assertEqual(value, 'fewsnet_rfe')
-
-             value = sds_meta.get_item('eStation2_scaling_factor')
-             self.assertEqual(value, '1.0')
-
-             value = sds_meta.get_item('eStation2_unit')
-             self.assertEqual(value, None)
-
-        else:
-            logger.info('Test file not existing: skip test')
-
-    def test_assign_from_product(self):
-
-
-        first_input = '/data/processing/vgt-ndvi/sv2-pv2.1/SPOTV-Africa-1km/tif/ndv/19990101_vgt-ndvi_ndv_SPOTV-Africa-1km_sv2-pv2.1.tif'
-        sds_meta = SdsMetadata()
-        output_file='/data/processing/vgt-ndvi/sv2-pv2.1/SPOTV-Africa-1km/derived/ndvi-linearx1/19981221_vgt-ndvi_ndvi-linearx1_SPOTV-Africa-1km_sv2-pv2.1.tif'
-        # Open and read data
-        sds_meta.read_from_file(first_input)
-
-        # Modify/Assign some to the ingested file
-        sds_meta.assign_comput_time_now()
-        str_date, productcode, subproductcode, mapset, version = functions.get_all_from_filename(os.path.basename(output_file))
-
-        #productcode='vgt-ndvi'
-        #subproductcode='ndvi_linearx1'
-        #version='sv2-pv2.1'
-        sds_meta.assign_from_product(productcode, subproductcode, version)
-        sds_meta.print_out()
